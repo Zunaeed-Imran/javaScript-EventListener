f1e870db8f1f1b79adecea89e85b9081c68ff77a
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-22 10:24:21 +0600

    multiple button event listener use for loop
    multiple button event listener use for loop

---
+++ ButtonLoop.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 3; i++){
+document.querySelectorAll(".button")[i].addEventListener("click", function () {
+  var text = this.innerHTML;
+  document.querySelector("h1").innerHTML = text + " is clicked";
+});
+}
